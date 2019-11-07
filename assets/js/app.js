5dda6020caabe273e30a4d4920b0cfe10604131b
David <arattanasack@gmail.com>
2019-11-07 14:44:06 -0800

    Adds working search button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,16 @@
 //javascript, jQuery
-// let searchInput = $('#searchinput').val().trim();
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=dogs&api_key=kI0T0d8hy8iGLfpIMHqAVeGpN5MFUspg&limit=10");
+const getData = () => {
+    let searchInput = $('#searchinput').val().trim();
+    let xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + searchInput + "&api_key=kI0T0d8hy8iGLfpIMHqAVeGpN5MFUspg&limit=10");
 xhr.done(function(response) { 
     console.log("success got data", response); 
 let gifs = response.data;
-
+$('#gifs-go-here').empty();
 for (let i = 0; i < gifs.length; i++) {
-    $('#gifs-go-here').append("<img src='" + gifs[i].images.original.url + "'/>")
+    let gifDisplay = $("<div>");
+    gifDisplay.append("<img src='" + gifs[i].images.original.url + "'/>");
+    gifDisplay.append("Rating: " + gifs[i].rating);
+    $('#gifs-go-here').append(gifDisplay);
 }
 });
+};
