491eefdc49cff211c74f53b3ce79b5c46f73bc3e
David <arattanasack@gmail.com>
2019-11-07 16:37:19 -0800

    [WIP] Adds click handler to card-img-top

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,11 +15,31 @@ const getData = () => {
         $('.gif-container').empty();
         for (let i = 0; i < gifs.length; i++) {
             let gifDisplay = $("<div>");
-            gifDisplay.addClass('card gifBlock');
+            gifDisplay.addClass('card gifBlock rounded');
             gifDisplay.append("<img class='card-img-top' src='" + gifs[i].images.original.url + "'>");
+            // gifDisplay.append("<img class='card-img-top' src='" + gifs[i].images.original_still.url + "'>");
             gifDisplay.append("<p class='card-text'>Title: " + gifs[i].title + "</p>");
             gifDisplay.append("<p class='card-text'>Rating: " + gifs[i].rating + "</p>");
-            $('.gif-container').append(gifDisplay);
+            // gifDisplay.attr('data-still', gifs[i].images.original_still.url);
+            // gifDisplay.attr('data-animate', gifs[i].images.original.url);
+            // gifDisplay.attr('data-state', 'still');
+            // gifDisplay.attr('id', 'gifClick');
+            // $('.card-img-top').css('cursor', 'pointer');
+            $('#gif-container').append(gifDisplay);
         }
     });
+    
 };
+
+//[WIP] Play GIF on click
+$('#gifClick').on('click', function(){
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+      console.log(this.state);
+});
